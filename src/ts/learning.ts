cdba59cdbbe2a3858b8f6eedcff8c55f0f9b41a1
kuwahara <kuwahara@lepra.jp>
2016-03-17 18:25:28 +0900

    delete twitter.js

--- src/ts/learning.ts
+++ src/ts/learning.ts
@@ -31,6 +31,16 @@ class Learning
          access_token_key:    config.access_token_key,
          access_token_secret: config.access_token_secret,
       });
+
+      // search by key word
+      // ※only japanese text
+      client.get('/search/tweets.json', {"q":"", "count": 100}, function(err, data) {
+         data.statuses.forEach(function(info) {
+            if (info.text.match(/^[ぁ-んァ-ン一-龠]+$/)) {
+               console.log(info.text);
+            }
+         });
+      });
   }
 
    /**
