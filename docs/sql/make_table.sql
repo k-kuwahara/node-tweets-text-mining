30665866cf036c994413543d75d4ead4573935e1
kuwahara <kuwahara@lepra.jp>
2016-03-18 18:37:42 +0900

    add initail data

--- docs/sql/make_table.sql
+++ docs/sql/make_table.sql
@@ -1,8 +1,8 @@
 # create database
-CREATE DATABASE `db_name` CHARACTER SET utf8;
+CREATE DATABASE `nodejs` CHARACTER SET utf8;
 
 # set db_name
-use db_name;
+use nodejs;
 
 # create tables
 CREATE TABLE `word_count`(
@@ -10,7 +10,6 @@ CREATE TABLE `word_count`(
    `word`      VARCHAR(255)             NOT NULL COMMENT '単語',
    `label`     VARCHAR(255)             NOT NULL COMMENT 'ラベル',
    `count`     INT(11)      DEFAULT 0   NOT NULL COMMENT '出現回数',
-   `dimension` SMALLINT(6)  DEFAULT 140          COMMENT '次元',
    PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8 COMMENT='単語';
 
@@ -20,3 +19,40 @@ CREATE TABLE `weight_values`(
    `value`     INT(11)          DEFAULT 1 NOT NULL COMMENt '重みベクトルの値',
    PRIMARY KEY (`weight_id`)
 ) DEFAULT CHARSET=utf8 COMMENT='重みベクトルの値';
+
+# initial datas
+INSERT INTO `word_count`(`word`, `label`, `count`) VALUES
+('好き', '1', '0'),
+('すき', '1', '0'),
+('スキ', '1', '0'),
+('良い', '1', '0'),
+('いい', '1', '0'),
+('イイ', '1', '0'),
+('凄い', '1', '0'),
+('すごい', '1', '0'),
+('スゴイ', '1', '0'),
+('すげえ', '1', '0'),
+('スゲエ', '1', '0'),
+('すげぇ', '1', '0'),
+('スゲェ', '1', '0'),
+('やばい', '1', '0'),
+('ヤバイ', '1', '0'),
+('嫌い', '-1', '0'),
+('きらい', '-1', '0'),
+('キライ', '-1', '0'),
+('悪い', '-1', '0'),
+('わるい', '-1', '0'),
+('ワルイ', '-1', '0'),
+('駄目', '-1', '0'),
+('だめ', '-1', '0'),
+('ダメ', '-1', '0'),
+('酷い', '-1', '0'),
+('ひどい', '-1', '0'),
+('ヒドイ', '-1', '0'),
+('ダメ', '-1', '0'),
+('嫌', '-1', '0'),
+('いや', '-1', '0'),
+('イヤ', '-1', '0'),
+('あかん', '-1', '0'),
+('アカン', '-1', '0');
+
