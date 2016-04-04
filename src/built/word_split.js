395c308711f23902000f9f4a07a6d8a591e7807b
k-kuwahara <zensin0082@gmail.com>
2016-04-04 11:54:58 +0900

    add error handling for word_split

--- src/built/word_split.js
+++ src/built/word_split.js
@@ -1,3 +1,5 @@
+/// <reference path="../ts/node.d.ts" />
+/// <reference path="../ts/xml2js.d.ts" />
 "use strict";
 var config = require('./config');
 var http = require('http');
@@ -14,11 +16,19 @@ function split(text) {
     };
     http.get(options, function (res) {
         res.setEncoding('utf8');
+        res.on('error', function (e) {
+            console.log(e.message);
+        });
         res.on('data', function (data) {
             parse(data, function (err, result) {
-                result.ResultSet.ma_result[0].word_list[0].word.forEach(function (word) {
-                    words.push(word.surface);
-                });
+                if (!err || result !== undefined) {
+                    result.ResultSet.ma_result[0].word_list[0].word.forEach(function (word) {
+                        words.push(word.surface);
+                    });
+                }
+                else {
+                    console.log(err.message);
+                }
             });
         });
     }).on('error', function (e) {
--- src/ts/word_split.ts
+++ src/ts/word_split.ts
@@ -1,5 +1,5 @@
-/// <reference path="./node.d.ts" />
-/// <reference path="./xml2js.d.ts" />
+/// <reference path="../ts/node.d.ts" />
+/// <reference path="../ts/xml2js.d.ts" />
 
 import config = require('./config');
 import http   = require('http');
@@ -19,16 +19,23 @@ export function split(text: string): string[]
      method: 'GET'
   };
 
-  http.get(options, function(res) {
+  http.get(options, (res) => {
      res.setEncoding('utf8');
-     res.on('data', function(data) {
-        parse(data, function(err, result) {
-           result.ResultSet.ma_result[0].word_list[0].word.forEach(function(word) {
-              words.push(word.surface);
-           });
+     res.on('error', (e) => {
+        console.log(e.message);
+     });
+     res.on('data', (data) => {
+        parse(data, (err, result) => {
+           if (!err || result !== undefined) {
+              result.ResultSet.ma_result[0].word_list[0].word.forEach(function(word) {
+                 words.push(word.surface);
+              });
+           } else {
+              console.log(err.message);
+           }
         });
      });
-  }).on('error', function(e) {
+  }).on('error', (e) => {
      console.log(e.message);
   });
   return words;
